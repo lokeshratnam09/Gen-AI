c1e55c0aac7d791e45c84c7ff3cdb0a73ebc2c71
lokeshratnam09 <lokeshratnam09@gmail.com>
2026-05-22 18:56:10 +0530

    Implement Plotting ..

--- Basic/graphs_ex.py
+++ Basic/graphs_ex.py
@@ -56,4 +56,41 @@ import pandas as pd
 # plt.show()
 
 
+# subjects = ["Math", "Science", "English", "History"]
+# marks = [85, 90, 78, 92]
+# colors = ["red", "blue", "green", "orange"]
+# explode = (0,0,1,0)
+# plt.figure(figsize=(8,8))
+# plt.pie(marks,
+#         labels=subjects,
+#         colors=colors,
+#         explode=explode,
+#         autopct='%1.1f%%',
+#         shadow=True,
+#         startangle=90)
+# plt.title("Marks Distribution")
+# plt.title("Student Marks Distribution")
+# plt.legend(title="Subjects")
+# plt.show()
+
+
+
+# Scatter Plot
+# study_hours = [1,2,3,4,5,6,7,8]
+# marks = [35,40,50,60,65,70,85,95]
+# sizes = [100,120,140,160,180,200,220,240]
+# colors = ['red', 'blue', 'green', 'orange', 'purple', 'cyan', 'magenta', 'yellow']
+# plt.figure(figsize=(10,6))
+# plt.scatter(study_hours,marks,s=sizes,c=colors,alpha=0.5,edgecolor='black',marker='o')
+# plt.title("Study Hours vs Marks Analysis",fontsize=16)
+# plt.xlabel("Study Hours",fontsize=12)
+# plt.ylabel("Marks",fontsize=12)
+# plt.annotate('Top Student',xy=(8,95),xytext=(8,80),arrowprops=dict(facecolor='red', shrink=0.10))
+# plt.grid(True)
+# plt.show()
+
+
+
+
+
 
